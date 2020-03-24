e47293b921f7bff9edea17dc037cb70cdb407b7c
obaocr <for.oba@outlook.fr>
2020-03-24 12:03:40 +0100

    init

---
+++ src/main/java/com/safetynet/p5_alerts/P5AlertsApplication.java
@@ -0,0 +1,13 @@
+package com.safetynet.p5_alerts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P5AlertsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(P5AlertsApplication.class, args);
+	}
+
+}
